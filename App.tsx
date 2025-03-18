36eedbf5de2a9214d4f9baf3991dc6d69a208b10
woo <khw19980926@gmail.com>
2025-03-18 17:42:51 +0900

    tab Navigation study

--- App.tsx
+++ App.tsx
@@ -1,21 +1,52 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {createDrawerNavigator} from "@react-navigation/drawer";
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet,} from 'react-native';
 import UserScreen from './screens/UserScreen';
-
-const Draw = createDrawerNavigator();
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MailScreen from './screens/MailScreen';
+import {Ionicons} from "@expo/vector-icons"
+import HomeScreen from './screens/HomeScreen';
+const TabStack = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Draw.Navigator>
-        <Draw.Screen name='user' options={{
-          drawerLabel:"회원유저",
-          drawerActiveBackgroundColor:"orange",
-        }} component={UserScreen}/>
-      </Draw.Navigator>
-    </NavigationContainer>  
+    <SafeAreaView style={{flex:1,}}>
+      <NavigationContainer>
+        <TabStack.Navigator
+          screenOptions={{
+            tabBarActiveTintColor: "red",
+            headerShown: false,
+          }}
+        >
+          <TabStack.Screen
+            name="home"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({ size, color }) => (
+                <Ionicons name="home" size={size} color={color} />
+              ),
+            }}
+          />
+          <TabStack.Screen
+            name="user"
+            component={UserScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => {
+                return <Ionicons name="person" size={size} color={color} />;
+              },
+            }}
+          />
+          <TabStack.Screen
+            name="mail"
+            component={MailScreen}
+            options={{
+              tabBarIcon: ({ size, color }) => (
+                <Ionicons name="mail" size={size} color={color} />
+              ),
+            }}
+          />
+        </TabStack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
