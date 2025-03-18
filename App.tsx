8d0375cc87d90bb4b47a1ecc35e85f5e1bc63539
woo <khw19980926@gmail.com>
2025-03-18 17:59:01 +0900

    fix

--- App.tsx
+++ App.tsx
@@ -1,17 +1,32 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, View } from 'react-native';
-import CategoryScreen from './screens/CategoryScreen';
-import MealsOverviewScreen from './screens/MealsOverviewScreen';
-import MealDetailScreen from './screens/MealDetailScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, Text, View } from "react-native";
+import CategoryScreen from "./screens/CategoryScreen";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealDetailScreen from "./screens/MealDetailScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Stack = createNativeStackNavigator();
+const DrawerStack = createDrawerNavigator();
+// function DrawerStack() {
+//   return (
+//     <Drawer.Navigator>
+//       <Drawer.Screen 
+//         name="category" 
+//         component={CategoryScreen} 
+//         options={{
+//           title:"Category"
+//         }} 
+//       />
+//     </Drawer.Navigator>
+//   );
+// }
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="category"
+        initialRouteName="drawer"
         screenOptions={{
           headerStyle: { backgroundColor: "#351401" },
           headerTitleStyle: { color: "white" },
@@ -19,17 +34,18 @@ export default function App() {
         }}
       >
         <Stack.Screen
-          name="category"
+          name="drawer"
           component={CategoryScreen}
           options={{
-            title: "All Category",
+            title: "Main Category",
+            headerShown: false,
           }}
         />
         <Stack.Screen
           name="mealDetail"
           component={MealDetailScreen}
           options={{
-            headerRight:()=><Text>In the header</Text>
+            headerRight: () => <Text>In the header</Text>,
           }}
         />
         <Stack.Screen name="mealsOverview" component={MealsOverviewScreen} />
@@ -39,7 +55,5 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-   
-  },
+  container: {},
 });
